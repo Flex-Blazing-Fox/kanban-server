8d40e68aa1c9f4fdf8494dec04862d96f544d456
Rangga Putra Pertama <ranggarppb@MacBook-Pro-Rangga.local>
2021-07-12 01:30:19 +0700

    add allow ssl settings in config

--- config/config.js
+++ config/config.js
@@ -1,25 +1,32 @@
-require('dotenv').config();
+require("dotenv").config();
 module.exports = {
-  "development": {
-      "username": process.env.DB_USERNAME_DEV,
-      "password": process.env.DB_PASSWORD_DEV,
-      "database": process.env.DB_DATABASE_DEV,
-      "host": process.env.DB_HOST_DEV,
-      "port": process.env.DB_PORT_DEV,
-      "dialect": "postgres"
+  development: {
+    username: process.env.DB_USERNAME_DEV,
+    password: process.env.DB_PASSWORD_DEV,
+    database: process.env.DB_DATABASE_DEV,
+    host: process.env.DB_HOST_DEV,
+    port: process.env.DB_PORT_DEV,
+    dialect: "postgres",
   },
-  "test": {
-    "username": "postgres",
-    "password": "postgres",
-    "database": "postgres",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+  test: {
+    username: "postgres",
+    password: "postgres",
+    database: "postgres",
+    host: "127.0.0.1",
+    dialect: "postgres",
   },
-  "production": {
-    "username": process.env.DB_USERNAME_PROD,
-    "password": process.env.DB_PASSWORD_PROD,
-    "database": process.env.DB_DATABASE_PROD,
-    "host": process.env.DB_HOST_PROD,
-    "dialect": "postgres"
-  }
-  };
+  production: {
+    username: process.env.DB_USERNAME_PROD,
+    password: process.env.DB_PASSWORD_PROD,
+    database: process.env.DB_DATABASE_PROD,
+    host: process.env.DB_HOST_PROD,
+    dialect: "postgres",
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+};
