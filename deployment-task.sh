6b79e72aa6aae20fa30d952090fa3d173baa4007
Rangga Putra Pertama <ranggarppb@MacBook-Pro-Rangga.local>
2021-07-12 22:56:13 +0700

    fix bug in migration command in release heroku

--- deployment-task.sh
+++ deployment-task.sh
@@ -1,5 +1,4 @@
 npm install --only=prod
-npx sequelize db:migrate:undo:all --env production
-npx sequelize db:migrate:all --env production
+npx sequelize db:migrate --env production
 npx sequelize db:seed:undo:all --env production
 npx sequelize db:seed:all --env production
