9b665afd340c0ba3a6e446a239edcb93c3bef7ff
Rangga Putra Pertama <ranggarppb@MacBook-Pro-Rangga.local>
2021-07-12 23:05:14 +0700

    install all package.json

--- deployment-task.sh
+++ deployment-task.sh
@@ -1,4 +1,4 @@
-npm install --only=prod
+npm install package.json
 npx sequelize db:migrate --env production
 npx sequelize db:seed:undo:all --env production
 npx sequelize db:seed:all --env production
