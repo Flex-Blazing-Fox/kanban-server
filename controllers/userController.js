a477e288c017b0dae279a3bf8621281b38115f62
Rangga Putra Pertama <ranggarppb@MacBook-Pro-Rangga.local>
2021-07-12 23:25:59 +0700

    logging in login

--- controllers/userController.js
+++ controllers/userController.js
@@ -24,6 +24,7 @@ class userController {
   }
   static login(req, res, next) {
     const { email, password } = req.body;
+    console.log(req.body)
     User.findOne({
       where: {
         email: email,
