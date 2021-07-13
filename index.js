9fb9940033bb4fc1f056d102b63c35adcd36f898
Rangga Putra Pertama <ranggarppb@MacBook-Pro-Rangga.local>
2021-07-13 22:53:33 +0700

    add websocket basic

--- index.js
+++ index.js
@@ -3,15 +3,32 @@ const express = require("express");
 const routers = require("./routers");
 const errorHandler = require("./middlewares/errorHandler");
 const PORT = process.env.PORT || 3000;
-const cors = require("cors")
+const cors = require("cors");
+const WebSocket = require("ws");
 
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routers);
 app.use(errorHandler);
 
-module.exports = app.listen(PORT, () => {
+const server = require("http").createServer(app);
+const wss = new WebSocket.Server({ server: server });
+wss.on("connection", function connection(ws) {
+  ws.on("message", function incoming(message) {
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+});
+
+// module.exports = app.listen(PORT, () => {
+//   console.log(`listening to port ${PORT}`);
+// });
+
+server.listen(PORT, () => {
   console.log(`listening to port ${PORT}`);
 });
