386be135c8a30d2d0f1dd2fcb54fa11dc099f4ef
ardiansyakh <abdullah.ard9@gmail.com>
2021-07-23 07:40:20 +0700

    config prod

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-require('dotenv').config()
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
 const express = require('express')
 const errHandler = require('../fancy-todo-server/middleware/errHandler')
 const app = express()
